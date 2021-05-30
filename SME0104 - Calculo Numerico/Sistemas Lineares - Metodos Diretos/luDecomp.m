f9f47f983a121a38724363eb53e883ae1f97ce2e
andrecreppe <andrecrepper@gmail.com>
2021-05-30 15:11:35 -0300

    Calculo Numerico - Aula 2
    Códigos para a Aula 2 feitos durante o estudo da P1

---
+++ SME0104 - Calculo Numerico/Sistemas Lineares - Metodos Diretos/luDecomp.m
@@ -0,0 +1,15 @@
+function [L,U] = lu decomp(A)
+  % A: matriz quadrada
+  % L, U: matrizes triang. inf. e sup., respectivamente
+  n = size(A,1);
+  L = eye(n); U = zeros(n);
+  
+  for k=1:n
+    for j=k:n
+      U(k,j) = A(k,j) - L(k,1:k-1) * U(1:k-1,j);
+    end
+    
+    for i=k+1:n
+      L(i,k) = (A(i,k) - L(i,1:k-1)*U(1:k-1,k))/U(k,k);
+    end
+end
---
+++ SME0104 - Calculo Numerico/Sistemas Lineares - Metodos Diretos/subProgressiva.m
@@ -0,0 +1,1 @@
+function x = subProgressiva(L, b)  % L: matriz triangular inferior  % b: termo independente  % x: vetor solucao  n = length(b);  x = zeros(n,1);    for i=1:n    x(i) = (b(i) - L(i,1:i-1)*x(1:i-1))/L(i,i);end
