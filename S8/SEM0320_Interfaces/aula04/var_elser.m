ba5dbb2f9fa6a83472b37880793c056ba97200b8
André Creppe <andrecrepper@gmail.com>
2024-08-31 14:52:13 -0300

    IEM - Aula Prática 04

---
+++ S8/SEM0320_Interfaces/aula04/var_elser.m
@@ -0,0 +1,23 @@
+% Sistema Atuador Elástico-Serial
+
+close all; clear all
+clc
+
+La = 8.2e-4;
+Ra = 1.03;
+JT = 9.51e-5;
+M = 1.261;
+CT = 4.594e-5;
+KH = 3.336e6;
+Kb = 1.469e-1;
+KL = 3.303e6;
+CL = 8.08;
+K = 3.27e4;
+C = 8.0e-1;
+KT = 1.470e-1;
+n = 0.001;
+p = 0.003;
+
+Kp = 2.915;
+Ki = 4.129;
+Kd = 0.007;
---
+++ S8/SEM0320_Interfaces/aula04/var_mma.m
@@ -0,0 +1,9 @@
+% Sistema Massa-Mola-Amortecedor
+
+close all; clear all
+clc
+
+m = 1;
+k = 1;
+B = 1e-1;
+f = 10;
