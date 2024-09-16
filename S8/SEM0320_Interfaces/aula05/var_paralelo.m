8cf97e935cb26c74dc66732aa77465b222219f70
andrecreppe <andrecrepper@gmail.com>
2024-09-16 16:09:28 -0300

    IEM - Aula 05

---
+++ S8/SEM0320_Interfaces/aula05/var_paralelo.m
@@ -0,0 +1,9 @@
+% Sistema resistor-indutor-capacitor em paralelo
+
+clear all; close all;
+clc
+
+L = 1;
+C = 1e-3;
+R = 100;
+i = 1;
---
+++ S8/SEM0320_Interfaces/aula05/var_serie.m
@@ -0,0 +1,9 @@
+% Sistema resistor-indutor-capacitor em série
+
+clear all; close all;
+clc
+
+L = 1;
+C = 1e-3;
+R = 5;
+V = 10;
